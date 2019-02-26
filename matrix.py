0e2ef0c923b652d19cc0847baa94c332c1b4d875
nadiaristya <47979374+nadiaristya@users.noreply.github.com>
2019-02-26 22:39:03 +0700

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -7,9 +7,11 @@ class matrix:
         matriks = []
         for i in range(x):
             for j in range(y):
-                a = int(input("Masukkan baris ke {} dan kolom ke {} :".format(j,i)))
+                a = int(input("Masukkan baris ke {} dan kolom ke {} :".format(i,j)))
                 array.append(a)
+                print(array)
             matriks.append(array)
+            print(matriks)
             array = []
         return np.asarray(matriks)
 
@@ -26,9 +28,9 @@ class matrix:
                 print("Ordo yang anda masukkan tidak sesuai")
             else : 
                 print("Masukkan matriks 1 : ")
-                matriks1 = inmatrix(kolom_m1, baris_m1)
+                matriks1 = matrix.inmatrix(baris_m1, kolom_m1)
                 print("Masukkan matriks 2 : ")
-                matriks2 = inmatrix(kolom_m2, baris_m2)
+                matriks2 = matrix.inmatrix(baris_m2, kolom_m2)
                 print(np.dot(matriks1,matriks2))
 
         except ValueError:
@@ -37,10 +39,10 @@ class matrix:
     def perkalianSkalar():
 
         b = int(input("Masukkan bilangan : "))
-        baris_m1 = int(input("Masukkan baris matriks 1 : "))
-        kolom_m1 = int(input("Masukkan kolom matriks 1 : "))
+        baris = int(input("Masukkan baris matriks : "))
+        kolom = int(input("Masukkan kolom matriks : "))
         print("Masukkan matriks : ")
-        matriks1 = inmatrix(kolom_m1, baris_m1)
+        matriks1 = matrix.inmatrix(baris, kolom)
         print(b*matriks1)
 
 def main():
@@ -51,10 +53,11 @@ def main():
         elif int (userInput) == 2:
             matrix.perkalianSkalar()
         else : 
-            print("anda bodoh")
+            print("Hanya dapat memasukkan pilihan 1 atau 2.")
     except ValueError:
-        print("Hanya bisa dimasukkan angka saja")
+        print("Hanya dapat memasukkan dalam bentuk angka saja.")
 
 
 if __name__ == "__main__": 
     main()
+
